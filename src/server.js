c76038218e02170aa8a5b9b1472efd02a00e1cef
js25s <petrauskas.justas@gmail.com>
2021-11-05 09:59:58 +0100

    basic init

---
+++ src/server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import {
+  notFoundHandler,
+  badRequestHandler,
+  genericErrorHandler,
+  unauthorizedHandler,
+  forbiddenHandler,
+  catchAllHandler,
+} from "./errorHandlers/errorHandlers.js";
+
+const server = express();
+const { PORT = 5000 } = process.env;
+
+// MIDDLEWARES
+
+server.use(cors());
+server.use(express.json());
+
+server.use(unauthorizedHandler);
+server.use(forbiddenHandler);
+server.use(catchAllHandler);
+server.use(notFoundHandler);
+server.use(badRequestHandler);
+server.use(genericErrorHandler);
+
+server.listen(PORT, async () => {
+  // connect to mongoose Server
+  mongoose.connect(process.env.MONGODB, {});
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+server.on("error", (error) => {
+  console.log("Server is stoppped ", error);
+});
